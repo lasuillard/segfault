26e8cb457b9358abbf969e8e66d4d315a67796f4
Rena <ckdwomn@live.com>
2019-10-01 23:05:25 +0900

    PWA : Native, Push Update
    * Native
    - Support Android, iOS, Windows
    - Update Icon
    - Update theme color
    
    * Push
    - Support get message

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -80,15 +80,45 @@ export default {
     manifest: {
       name: 'SegFault',
       short_name: 'SegFault',
+      description: 'Dev Community',
+      lang: 'ko-KR',
       start_url: '/',
       display: 'standalone',
-      background_color: '#F2F2F2',
+      theme_color: '#263238',
+      background_color: '#263238',
+      gcm_sender_id: '103953800507',
       icons: [{
-        "src": "/icon.png",
-        "sizes": "192x192",
-        "type": "image/png"
+        "src": "/icons/android-icon-36x36.png",
+        "sizes": "36x36",
+        "type": "image/png",
+        "density": "0.75"
       }, {
-        "src": "/icon.png",
+       "src": "/icons/android-icon-48x48.png",
+       "sizes": "48x48",
+       "type": "image/png",
+       "density": "1.0"
+      }, {
+        "src": "/icons/android-icon-72x72.png",
+        "sizes": "72x72",
+        "type": "image/png",
+        "density": "1.5"
+       }, {
+        "src": "/icons/android-icon-96x96.png",
+        "sizes": "96x96",
+        "type": "image/png",
+        "density": "2.0"
+       }, {
+        "src": "/icons/android-icon-144x144.png",
+        "sizes": "144x144",
+        "type": "image/png",
+        "density": "3.0"
+       }, {
+        "src": "/icons/android-icon-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png",
+        "density": "4.0"
+      }, {
+        "src": "/icons/icon.png",
         "sizes": "512x512",
         "type": "image/png"
       }]
@@ -115,4 +145,30 @@ export default {
     extend(config, ctx) {
     }
   },
+}
+
+module.exports = {
+  head: {
+    link: [
+      { rel: 'shortcut icon', href: '/icons/favicon.ico' }, 
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/icons/apple-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/icons/apple-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/icons/apple-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/icons/apple-icon-76x76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/icons/apple-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/icons/apple-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/icons/apple-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/icons/apple-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-icon-180x180.png' },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/icons/android-icon-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/icons/favicon-96x96.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' },
+    ],
+    meta: [
+      { name: 'msapplication-TileColor', content: '#263238' },
+      { name: 'msapplication-TileImage', content: '/icons/ms-icon-144x144.png' },
+      { name: 'theme-color', content: '#263238' }
+    ]
+  }
 }
